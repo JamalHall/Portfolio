7a642f704e76da3ab3ff820b6f8e55e112e200e6
JHall <JamalHallWebDev@gmail.com>
2023-07-09 18:36:25 -0400

    adjusted rounding

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ class DivGenerator {
       const char = str[i];
       const div = document.createElement("div");
       div.textContent = char;
-      div.classList.add("title", `delay${Math.ceil(Math.random() * 10)}`);
+      div.classList.add("title", `delay${Math.floor(Math.random() * 10)}`);
       this.divs.push(div);
     }
     return this.divs;
@@ -15,5 +15,4 @@ class DivGenerator {
 
 const generator = new DivGenerator();
 const container = generator.generateDivs("Jamal Hall Is Coming Soon");
-
 document.getElementById("container").append(...container);
